446c318b28451362da582e080b79b5b5e6c38fd1
Basalyko Andrey <basalykoa.s@gmail.com>
2022-07-18 18:30:50 +0300

    Сделал первую задачу.

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] arr = new int[5];
+int chet = 0;
+int hlp = 0;
+void Method(int a)
+{
+    if (a % 2 ==1)
+    {
+        // Нечетное
+    }
+    else
+    {
+        ++chet;
+    }
+}
+Random rand = new Random();
+for(int i =0;i<arr.Length;i++)
+   arr[i] = rand.Next(100,999);
+while (hlp < 5)
+{
+    Method(arr[hlp]);
+    Console.Write(arr[hlp]);
+    Console.Write(", ");
+    ++hlp;
+}
+Console.Write(" -> ");
+ Console.Write(chet);
